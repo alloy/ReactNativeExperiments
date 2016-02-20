ac757f6b408897dbd9bb0fa6b66e3485f07b7ee2
Eloy Durán <eloy.de.enige@gmail.com>
2016-02-20 01:21:31 +0100

    Remove Eigen masonry code, it's not used atm.

--- EmbedInExistingNavigationStack/EmbedInExistingNavigationStack/ARComponents/ARNativeMasonryGridComponent/Eigen/ARPostAttachment.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@protocol ARPostAttachment <NSObject>
-
-- (NSURL *)urlForThumbnail;
-- (CGFloat)aspectRatio;
-- (CGSize)maxSize;
-
-@end
--- EmbedInExistingNavigationStack/EmbedInExistingNavigationStack/ARComponents/ARNativeMasonryGridComponent/Eigen/UIImage+ImageFromColor.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-@interface UIImage (ImageWithColor)
-+ (UIImage *)imageFromColor:(UIColor *)color;
-+ (UIImage *)imageFromColor:(UIColor *)color withSize:(CGSize)size;
-@end
